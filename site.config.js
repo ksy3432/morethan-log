f1660da681c06dd0c69fd46b2be74eaacc983e85
ksy3432 <120364372+ksy3432@users.noreply.github.com>
2023-04-08 19:09:57 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "Seyeon Kim",
+    name: "ksy3432",
     image: "star.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "A student majoring computer science",
     bio: "Dream bigger, Act boldly.",
@@ -25,7 +25,7 @@ const CONFIG = {
 
   // CONFIG configration (required)
   link: "https://morethan-log.vercel.app",
-  since: 2023, // If leave this empty, current year will be used.
+  since: 2023~, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
   seo: {
