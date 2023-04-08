e0d3aca09096cc52cf5206285aaff70cfc1ce632
ksy3432 <120364372+ksy3432@users.noreply.github.com>
2023-04-08 18:51:03 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Seyeon Kim",
-    image: "/public/image/star.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/public/images/star.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "A student majoring computer science",
     bio: "Dream bigger, Act boldly.",
     email: "akasarao3@gmail.com",
